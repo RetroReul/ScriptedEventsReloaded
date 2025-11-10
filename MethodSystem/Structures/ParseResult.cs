aa666fec1176b8bffac39ceb43530ae8d4e04755
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-10 16:41:32 +0100

    Create ParseResult.cs

---
+++ MethodSystem/Structures/ParseResult.cs
@@ -0,0 +1,14 @@
+﻿using SER.ValueSystem;
+
+namespace SER.MethodSystem.Structures;
+
+public class ParseResult(Value? value)
+{
+    public Value? Value => value;
+    public bool Success => value != null;
+}
+
+public class ParseResult<T>(T? value) : ParseResult(value) where T : Value
+{
+    public new T? Value => value;
+}
