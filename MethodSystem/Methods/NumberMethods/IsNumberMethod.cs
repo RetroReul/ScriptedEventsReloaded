4eb3c778e61f4dad3eea1a5991015ef37e73a827
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-10 16:41:25 +0100

    Create IsNumberMethod.cs

---
+++ MethodSystem/Methods/NumberMethods/IsNumberMethod.cs
@@ -0,0 +1,21 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.NumberMethods;
+
+public class IsNumberMethod : ReturningMethod<BoolValue>
+{
+    public override string Description => "Returns true if the provided value is a number.";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new AnyValueArgument("value to check")
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = Args.GetAnyValue("value to check") is NumberValue;
+    }
+}
