d916f2ae6d66312a4fe25e5a5b634843661724d3
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 11:33:26 +0100

    Create CreateDBMethod.cs

---
+++ MethodSystem/Methods/DatabaseMethods/CreateDBMethod.cs
@@ -0,0 +1,21 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.FileSystem.Structures;
+using SER.MethodSystem.BaseMethods;
+
+namespace SER.MethodSystem.Methods.DatabaseMethods;
+
+public class CreateDBMethod : SynchronousMethod
+{
+    public override string Description => "Creates a new JSON file in the database folder.";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new TextArgument("name"),
+    ];
+    
+    public override void Execute()
+    {
+        Database.Create(Args.GetText("name"));
+    }
+}
