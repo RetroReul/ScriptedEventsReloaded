a89f3fe84d57821b6673b82dfb9136cb1f31dd6a
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 14:45:02 +0100

    Create DBHasKeyMethod.cs

---
+++ MethodSystem/Methods/DatabaseMethods/DBHasKeyMethod.cs
@@ -0,0 +1,22 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.DatabaseMethods;
+
+public class DBHasKeyMethod : ReturningMethod<BoolValue>
+{
+    public override string? Description => "Returns true if the provided key exists in the database.";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new DatabaseArgument("database"),
+        new TextArgument("key")   
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = Args.GetDatabase("database").HasKey(Args.GetText("key")).WasSuccessful();
+    }
+}
