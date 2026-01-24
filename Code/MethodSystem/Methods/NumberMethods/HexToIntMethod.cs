5a9e0c90d8bf2ded8fd926432d6955b092e15e9b
Tosoks67 <tosoks67@gmail.com>
2026-01-24 12:59:36 +0100

    hex to int and vice versa conversion methods

---
+++ Code/MethodSystem/Methods/NumberMethods/HexToIntMethod.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Exiled.Permissions.Commands.Permissions;
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Helpers.Exceptions;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.MethodSystem.MethodDescriptors;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.NumberMethods;
+
+[UsedImplicitly]
+public class HexToIntMethod : ReturningMethod<NumberValue>, ICanError, IAdditionalDescription
+{
+    public override string Description => "Parses a hexadecimal number back to a number value";
+
+    public string AdditionalDescription => "Do not include the '#' symbol at the start of the text.";
+
+    public string[] ErrorReasons =>
+    [
+        "The provided string does not represent a hexadecimal number."
+    ];
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new TextArgument("hex number")
+    ];
+
+    public override void Execute()
+    {
+        ReturnValue = int.TryParse(
+            Args.GetText("hex number"), 
+            NumberStyles.HexNumber, 
+            NumberFormatInfo.InvariantInfo, 
+            out var result) 
+            ? result 
+            : throw new ScriptRuntimeError(this, ErrorReasons[0]);
+    }
+}
---
+++ Code/MethodSystem/Methods/NumberMethods/IntToHexMethod.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.NumberMethods;
+
+[UsedImplicitly]
+public class IntToHexMethod : ReturningMethod<TextValue>
+{
+    public override string Description => "Parses an integer into a hexadecimal number";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new IntArgument("integer to parse")
+    ];
+
+    public override void Execute()
+    {
+        ReturnValue = Args.GetInt("integer to parse").ToString("X");
+    }
+}
