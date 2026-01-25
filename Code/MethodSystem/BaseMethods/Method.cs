7a9889f43ddea90ee4670d9e182b6cfd9349ef92
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-01-25 13:57:07 +0100

    add method name checking during compilation

--- Code/MethodSystem/BaseMethods/Method.cs
+++ Code/MethodSystem/BaseMethods/Method.cs
@@ -26,7 +26,7 @@ public abstract class Method
         Subgroup = type.Namespace?
             .Split('.')
             .LastOrDefault()?
-            .WithCurrent(name => name.Substring(0, name.Length - "Methods".Length)) 
+            .WithCurrent(name => name[..^"Methods".Length]) 
                    ?? "Unknown";
         
         var name = type.Name;
@@ -35,7 +35,7 @@ public abstract class Method
             throw new AndrzejFuckedUpException($"Method class name '{name}' must end with 'Method'.");
         }
         
-        Name = name.Substring(0, name.Length - "Method".Length);
+        Name = name[..^"Method".Length];
         Args = new(this);
     }
 
