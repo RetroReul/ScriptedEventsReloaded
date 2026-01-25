3be05530fa0b5490af4eea428cb026e126f28611
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-01-25 18:27:09 +0100

    Update GetWaveTimerMethod.cs

--- Code/MethodSystem/Methods/RespawnMethods/GetWaveTimerMethod.cs
+++ Code/MethodSystem/Methods/RespawnMethods/GetWaveTimerMethod.cs
@@ -24,7 +24,6 @@ public class GetWaveTimerMethod : ReferenceReturningMethod<Result<DurationValue>
         )
     ];
     
-    
     public override void Execute()
     {
         float duration = Args.GetOption("wave type") switch
