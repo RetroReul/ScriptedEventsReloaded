6dab664dcc4278cd7db70af030df3ec44ca5d6af
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-02-03 15:52:57 +0100

    Create SafeStartScript.cs

---
+++ Code/Examples/SafeStartScript.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+
+namespace SER.Code.Examples;
+
+[UsedImplicitly]
+public class SafeStartScript : Example
+{
+    public override string Name => "safeStart";
+
+    public override string Content =>
+        """
+        !-- OnEvent RoundStarted
+        
+        if {AmountOf @all} isnt 1
+            stop
+        end
+        
+        Print "SER enabled roundlock!"
+        Broadcast @all 10s "<color=green>SER enabled roundlock!"
+        RoundLock true
+        """;
+}
