4146ace20905f4fff8cb26c4faab8103a90d9a85
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-02-03 15:53:23 +0100

    Create ToFlagsMethod.cs

---
+++ Code/MethodSystem/Methods/ParsingMethods/ToFlagsMethod.cs
@@ -0,0 +1,60 @@
+﻿using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Exceptions;
+using SER.Code.Extensions;
+using SER.Code.Helpers.ResultSystem;
+using SER.Code.MethodSystem.BaseMethods;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+
+namespace SER.Code.MethodSystem.Methods.ParsingMethods;
+
+[UsedImplicitly]
+public class ToFlagsMethod : ReferenceReturningMethod<ToFlagsMethod.Flags>
+{
+    public record Flags(Type EnumType, Enum FlagEnum)
+    {
+        public TryGet<T> To<T>() where T : struct, Enum
+        {
+            try
+            {
+                return (T)FlagEnum;
+            }
+            catch
+            {
+                return $"Expected a '{EnumType.AccurateName}' enum type, got '{typeof(T).AccurateName}' type instead.";
+            }
+        }
+
+        public static string ErrorReason(Type enumType) => $"Expected a '{enumType.AccurateName}' enum type.";
+    };
+
+    public override string Description => "Parses wanted values into a requested enum flag.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new EnumTypeArgument("enum flag type"),
+        new TextArgument("values", false)
+        {
+            ConsumesRemainingValues = true
+        }
+    ];
+
+    public override void Execute()
+    {
+        var enumType = Args.GetEnumType("enum flag type");
+        var values = Args.GetRemainingArguments<string, TextArgument>("values");
+        long combinedValue = 0;
+
+        foreach (string val in values)
+        {
+            if (Enum.Parse(enumType, val, true) is Enum parsed)
+            {
+                // Convert to long to perform bitwise OR safely across different underlying types
+                combinedValue |= Convert.ToInt64(parsed);
+            }
+        }
+        
+        ReturnValue = new(enumType, (Enum)Enum.ToObject(enumType, combinedValue));
+    }
+}
