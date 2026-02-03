74092c87220bff3294b44b24acac4a169cd94e1e
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-02-03 15:52:49 +0100

    Create EnumTypeArgument.cs

---
+++ Code/ArgumentSystem/Arguments/EnumTypeArgument.cs
@@ -0,0 +1,25 @@
+﻿using AdminToys;
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Helpers.ResultSystem;
+using SER.Code.TokenSystem.Tokens;
+
+namespace SER.Code.ArgumentSystem.Arguments;
+
+public class EnumTypeArgument(string name, bool flagOnly = true) : Argument(name)
+{
+    public override string InputDescription => $"A name of an enum type, like {nameof(PrimitiveFlags)}";
+
+    [UsedImplicitly]
+    public DynamicTryGet<Type> GetConvertSolution(BaseToken token)
+    {
+        var name = token.RawRep;
+        Type? enumType = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .FirstOrDefault(t => t.Name == name && t.IsEnum && (!flagOnly || t.IsDefined(typeof(FlagsAttribute), false)));
+
+        return enumType == null 
+            ? $"There is no enum type called '{name}' {(flagOnly ? "that is a flag" : "")}."
+            : enumType;
+    }
+}
