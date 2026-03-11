49b3abc83adc16b1c07f7217da420e60289116f5
Tosoks67 <tosoks67@gmail.com>
2026-03-11 19:25:06 +0100

    type and stackTrace optional variables for on_error
    and also removal of ExceptionInfo as it's no longer needed

--- Code/ContextSystem/Contexts/Control/OnErrorStatement.cs
+++ Code/ContextSystem/Contexts/Control/OnErrorStatement.cs
@@ -8,6 +8,7 @@ using SER.Code.TokenSystem.Tokens;
 using SER.Code.TokenSystem.Tokens.VariableTokens;
 using SER.Code.ValueSystem;
 using SER.Code.VariableSystem.Bases;
+using SER.Code.VariableSystem.Variables;
 
 namespace SER.Code.ContextSystem.Contexts.Control;
 
@@ -15,8 +16,7 @@ namespace SER.Code.ContextSystem.Contexts.Control;
 public class OnErrorStatement : StatementContext, IStatementExtender, IKeywordContext, IAcceptOptionalVariableDefinitionsContext
 {
     public string KeywordName => "on_error";
-    public string Description =>
-        $"Catches an exception thrown inside of a {typeof(AttemptStatement).FriendlyTypeName(true)}";
+    public string Description => "Catches an exception thrown inside of an 'attempt' statement.";
     
     public string[] Arguments => [];
     public string Example =>
@@ -25,11 +25,22 @@ public class OnErrorStatement : StatementContext, IStatementExtender, IKeywordCo
         attempt
             Print {CollectionFetch &collection 2}
             # ERROR: there's nothing at index 2
+            
+            Print "Hello, world!"
+            # ^ won't get executed because 'attempt' skips the remaining code
+            # inside of it if an error was made
         on_error
-            with $message
+            with $message $type $stackTrace
             
             # this will print the error message
             Print "Error: {$message}"
+            
+            # In 90% of situations $type will be ScriptRuntimeError
+            Print "Type of error: {$type}"
+            
+            # This just shows where in the internal code (not the script) the error was made
+            # (basically to allow devs to know where in the code they may have fucked up)
+            Print "Stack trace: {$stackTrace}"
         end
         """;
 
@@ -46,7 +57,9 @@ public class OnErrorStatement : StatementContext, IStatementExtender, IKeywordCo
             field = value;
         }
     }
-    private LiteralVariableToken? _variableToken;
+    private VariableToken? _messageVariableToken;
+    private VariableToken? _typeVariableToken;
+    private VariableToken? _stackTraceVariableToken;
     
     public override TryAddTokenRes TryAddToken(BaseToken token)
     {
@@ -60,31 +73,79 @@ public class OnErrorStatement : StatementContext, IStatementExtender, IKeywordCo
 
     public Result SetOptionalVariables(params VariableToken[] variableTokens)
     {
-        if (variableTokens.Length > 1)
-            return $"Too many arguments provided for {FriendlyName}, only 1 is allowed.";
+        if (variableTokens.Length > 3)
+            return $"Too many arguments provided for {FriendlyName}, only up to 3 are allowed.";
 
-        if (variableTokens.First() is not LiteralVariableToken token)
+        var errorMsg = "Provided variable '{0}' cannot be used for an " + FriendlyName +
+                       $", as it's not a {typeof(LiteralVariable).FriendlyTypeName()}";
+
+        var messageToken = variableTokens.FirstOrDefault();
+        switch (messageToken)
         {
-            return $"{FriendlyName} only accepts a literal variable.";
+            case null:
+                return true;
+            case LiteralVariableToken:
+                _messageVariableToken = messageToken;
+                break;
+            default:
+                return string.Format(errorMsg, messageToken.RawRepr);
+        }
+
+        var typeToken = variableTokens.Skip(1).FirstOrDefault();
+        switch (typeToken)
+        {
+            case null:
+                return true;
+            case LiteralVariableToken:
+                _typeVariableToken = typeToken;
+                break;
+            default:
+                return string.Format(errorMsg, typeToken.RawRepr);
         }
         
-        _variableToken = token;
+        var stackTraceToken = variableTokens.Skip(2).FirstOrDefault();
+        switch (stackTraceToken)
+        {
+            case null:
+                break; // it's gonna return true either way
+            case LiteralVariableToken:
+                _stackTraceVariableToken = stackTraceToken;
+                break;
+            default:
+                return string.Format(errorMsg, stackTraceToken.RawRepr);
+        }
+        
         return true;
     }
     
     protected override IEnumerator<float> Execute()
     {
-        Variable? variable = null;
-        if (_variableToken is not null)
+        Variable? messageVariable = null;
+        Variable? typeVariable = null;
+        Variable? stackTraceVariable = null;
+        
+        if (_messageVariableToken is not null)
         {
-            variable = Variable.Create(_variableToken.Name, Value.Parse(Exception!.Message, Script));
-            Script.AddLocalVariable(variable);
+            messageVariable = Variable.Create(_messageVariableToken.Name, Value.Parse(Exception!.Message, Script));
+            Script.AddLocalVariable(messageVariable);
+        }
+        if (_typeVariableToken is not null)
+        {
+            typeVariable = Variable.Create(_typeVariableToken.Name, Value.Parse(Exception!.GetType().AccurateName, Script));
+            Script.AddLocalVariable(typeVariable);
+        }
+        if (_stackTraceVariableToken is not null)
+        {
+            stackTraceVariable = Variable.Create(_stackTraceVariableToken.Name, Value.Parse(Exception!.StackTrace, Script));
+            Script.AddLocalVariable(stackTraceVariable);
         }
 
         var coro = RunChildren();
         while (coro.MoveNext())
             yield return coro.Current;
         
-        if (variable is not null) Script.RemoveLocalVariable(variable);
+        if (messageVariable    is not null) Script.RemoveLocalVariable(messageVariable);
+        if (typeVariable       is not null) Script.RemoveLocalVariable(typeVariable);
+        if (stackTraceVariable is not null) Script.RemoveLocalVariable(stackTraceVariable);
     }
 }
