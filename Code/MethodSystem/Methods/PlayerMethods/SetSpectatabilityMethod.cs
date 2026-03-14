7315b260e71f629c0f908d4cc65c18b5ed9d6016
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-03-14 08:25:19 +0100

    Create SetSpectatabilityMethod.cs

---
+++ Code/MethodSystem/Methods/PlayerMethods/SetSpectatabilityMethod.cs
@@ -0,0 +1,29 @@
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+
+namespace SER.Code.MethodSystem.Methods.PlayerMethods;
+
+[UsedImplicitly]
+public class SetSpectatabilityMethod : SynchronousMethod
+{
+    public override string Description => "Allows or disallows a player to be spectated.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new PlayersArgument("players"),
+        new BoolArgument("new state")
+    ];
+    
+    public override void Execute()
+    {
+        var players = Args.GetPlayers("players");
+        var newState = Args.GetBool("new state");
+        
+        foreach (var player in players)
+        {
+            player.IsSpectatable = newState;
+        }
+    }
+}
