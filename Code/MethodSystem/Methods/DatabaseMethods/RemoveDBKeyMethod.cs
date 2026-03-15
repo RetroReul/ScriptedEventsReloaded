7f6f776e6677280e858f36afa4fd40770ea53bfe
Tosoks67 <tosoks67@gmail.com>
2026-03-15 16:44:18 +0100

    new RemoveDBKey method

---
+++ Code/MethodSystem/Methods/DatabaseMethods/RemoveDBKeyMethod.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+
+namespace SER.Code.MethodSystem.Methods.DatabaseMethods;
+
+[UsedImplicitly]
+public class RemoveDBKeyMethod : SynchronousMethod
+{
+    public override string Description => "Removes a key from a database.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new DatabaseArgument("database"),
+        new TextArgument("key")
+    ];
+    
+    public override void Execute()
+    {
+        Args.GetDatabase("database").RemoveKey(Args.GetText("key"));
+    }
+}
