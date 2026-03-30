408c0094e84e96955861dba866b68da53fa4f295
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-03-30 11:59:03 +0200

    Update EnumExtensions.cs

--- Code/Extensions/EnumExtensions.cs
+++ Code/Extensions/EnumExtensions.cs
@@ -9,4 +9,13 @@ public static class EnumExtensions
     {
         return new EnumValue<T>(enumValue);
     }
+    
+    public static IEnumerable<T> GetFlags<T>(this T value) where T : struct, Enum
+    {
+        return from T flag in Enum.GetValues(typeof(T)) 
+            where Convert.ToUInt64(value) != 0
+            where Convert.ToUInt64(flag) != 0 
+            where value.HasFlag(flag) 
+            select flag;
+    }
 }
