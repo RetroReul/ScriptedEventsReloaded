3708d4a6d7a874794a162f936a138d5c0ea0c5ea
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-03-30 09:13:20 +0200

    Update PlayerVariableDefinitionContext.cs

--- Code/ContextSystem/Contexts/VariableDefinition/PlayerVariableDefinitionContext.cs
+++ Code/ContextSystem/Contexts/VariableDefinition/PlayerVariableDefinitionContext.cs
@@ -1,9 +1,6 @@
-﻿using SER.Code.ContextSystem.Structures;
-using SER.Code.TokenSystem.Tokens;
-using SER.Code.TokenSystem.Tokens.VariableTokens;
+﻿using SER.Code.TokenSystem.Tokens.VariableTokens;
 using SER.Code.ValueSystem;
 using SER.Code.VariableSystem.Variables;
-using Log = SER.Code.Helpers.Log;
 
 namespace SER.Code.ContextSystem.Contexts.VariableDefinition;
 
