db35e7e9109bf6d2e928b9e31f0ab80876bc0f02
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 18:51:59 +0200

    SetRadioRange doesnt need exiled

--- Code/MethodSystem/Methods/ItemMethods/SetRadioRangeMethod.cs
+++ Code/MethodSystem/Methods/ItemMethods/SetRadioRangeMethod.cs
@@ -1,21 +1,15 @@
-using Exiled.API.Enums;
-using Exiled.API.Features.Items;
 using InventorySystem.Items.Radio;
 using JetBrains.Annotations;
 using SER.Code.ArgumentSystem.Arguments;
 using SER.Code.ArgumentSystem.BaseArguments;
-using SER.Code.Helpers;
 using SER.Code.MethodSystem.BaseMethods.Synchronous;
-using SER.Code.MethodSystem.Structures;
 using RadioItem = LabApi.Features.Wrappers.RadioItem;
 
 namespace SER.Code.MethodSystem.Methods.ItemMethods;
 
 [UsedImplicitly]
-public class SetRadioRangeMethod : SynchronousMethod, IDependOnFramework
+public class SetRadioRangeMethod : SynchronousMethod
 {
-    public FrameworkBridge.Type DependsOn => FrameworkBridge.Type.Exiled;
-    
     public override string Description => "Sets the radio range of a specified radio item.";
     
     public override Argument[] ExpectedArguments { get; } =
