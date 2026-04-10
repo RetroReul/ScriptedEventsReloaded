6d377555d99ca21137a6df3b5544becdab893049
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 18:38:45 +0200

    Create GetTeslasMethod.cs

---
+++ Code/MethodSystem/Methods/MapMethods/GetTeslasMethod.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using LabApi.Features.Wrappers;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.MapMethods;
+
+[UsedImplicitly]
+public class GetTeslasMethod : ReturningMethod<CollectionValue<ReferenceValue<Tesla>>>
+{
+    public override string Description => "Returns a collection of references to teslas.";
+
+    public override Argument[] ExpectedArguments { get; } = [];
+    
+    public override void Execute()
+    {
+        ReturnValue = new(Map.Teslas);
+    }
+}
