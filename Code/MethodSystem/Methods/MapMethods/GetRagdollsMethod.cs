d12f0a2b591f8dec60dde0991458a499044f43c8
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 18:38:38 +0200

    Create GetRagdollsMethod.cs

---
+++ Code/MethodSystem/Methods/MapMethods/GetRagdollsMethod.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using LabApi.Features.Wrappers;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.MapMethods;
+
+[UsedImplicitly]
+public class GetRagdollsMethod : ReturningMethod<CollectionValue<ReferenceValue<Ragdoll>>>
+{
+    public override string Description => "Returns a collection of references to ragdolls.";
+
+    public override Argument[] ExpectedArguments { get; } = [];
+    
+    public override void Execute()
+    {
+        ReturnValue = new(Map.Ragdolls);
+    }
+}
