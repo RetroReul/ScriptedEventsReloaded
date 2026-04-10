e2dd7a77ca8c2ca3e195e008ed67996f0b4c2508
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 17:30:20 +0200

    Create MuteMethod.cs

---
+++ Code/MethodSystem/Methods/PlayerMethods/MuteMethod.cs
@@ -0,0 +1,29 @@
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+
+namespace SER.Code.MethodSystem.Methods.PlayerMethods;
+
+[UsedImplicitly]
+public class MuteMethod : SynchronousMethod
+{
+    public override string Description => "Mutes specified players.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new PlayersArgument("players"),
+        new BoolArgument("is temporary")
+        {
+            DefaultValue = new(true, null)
+        }
+    ];
+    
+    public override void Execute()
+    {
+        var players = Args.GetPlayers("players");
+        var isTemporary = Args.GetBool("is temporary");
+        
+        foreach (var plr in players) plr.Mute(isTemporary);
+    }
+}
