368e466740edfa389b87a688d0c614d89148a23c
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 18:38:31 +0200

    Create GetDoorsMethod.cs

---
+++ Code/MethodSystem/Methods/MapMethods/GetDoorsMethod.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using LabApi.Features.Wrappers;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.MapMethods;
+
+[UsedImplicitly]
+public class GetDoorsMethod : ReturningMethod<CollectionValue<ReferenceValue<Door>>>
+{
+    public override string Description => "Returns a collection of references to doors.";
+
+    public override Argument[] ExpectedArguments { get; } = [];
+    
+    public override void Execute()
+    {
+        ReturnValue = new(Map.Doors);
+    }
+}
