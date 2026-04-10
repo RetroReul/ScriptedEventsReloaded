32b588b023b7d1762baaa67d60d1c9ed10080f00
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 18:38:36 +0200

    Create GetPickupsMethod.cs

---
+++ Code/MethodSystem/Methods/MapMethods/GetPickupsMethod.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using LabApi.Features.Wrappers;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.MapMethods;
+
+[UsedImplicitly]
+public class GetPickupsMethod : ReturningMethod<CollectionValue<ReferenceValue<Pickup>>>
+{
+    public override string Description => "Returns a collection of references to pickups.";
+
+    public override Argument[] ExpectedArguments { get; } = [];
+    
+    public override void Execute()
+    {
+        ReturnValue = new(Map.Pickups);
+    }
+}
