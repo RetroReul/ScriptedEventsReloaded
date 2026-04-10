93f180297a7ac22e285a5b987ac994600f349956
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-10 18:38:40 +0200

    Create GetRoomLightsMethod.cs

---
+++ Code/MethodSystem/Methods/MapMethods/GetRoomLightsMethod.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using LabApi.Features.Wrappers;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.MapMethods;
+
+[UsedImplicitly]
+public class GetRoomLightsMethod : ReturningMethod<CollectionValue<ReferenceValue<LightsController>>>
+{
+    public override string Description => "Returns a collection of references to room lights.";
+
+    public override Argument[] ExpectedArguments { get; } = [];
+    
+    public override void Execute()
+    {
+        ReturnValue = new(Map.RoomLights);
+    }
+}
