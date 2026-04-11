4deffbedc17124d5bfa88d95c9db0a1a8c988af2
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-11 22:31:33 +0200

    Create PlayerAnimatedBroadcastMethod.cs

---
+++ Code/MethodSystem/Methods/BroadcastMethods/PlayerAnimatedBroadcastMethod.cs
@@ -0,0 +1,53 @@
+using Cassie;
+using Exiled.API.Extensions;
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Helpers;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.MethodSystem.MethodDescriptors;
+using SER.Code.MethodSystem.Structures;
+using Player = Exiled.API.Features.Player;
+
+
+namespace SER.Code.MethodSystem.Methods.BroadcastMethods;
+
+[UsedImplicitly]
+public class PlayerAnimatedBroadcastMethod : SynchronousMethod, IAdditionalDescription, IDependOnFramework
+{
+    public override string Description => "Sends an animated broadcast to all players.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new PlayersArgument("players"),
+        new DurationArgument("duration"),
+        new TextArgument("content"),
+        new IntArgument("line break length")
+        {
+            Description = "How many characters are needed to make a new line",
+            DefaultValue = new(60, null)
+        }
+    ];
+
+    public override void Execute()
+    {
+        var content = Args.GetText("content");
+        var duration = Args.GetDuration("duration").TotalSeconds;
+
+        foreach (var plr in Args.GetPlayers("players").Select(Player.Get))
+        {
+            plr.Connection.Send(new CassieTtsPayload(string.Empty, string.Empty, false));
+            plr.MessageTranslated(
+                $"$SLEEP_{duration-1} .",
+                null!,
+                AnimatedBroadcastMethod.Helper.FormatToCassieCentralScreenSubtitles(content, Args.GetInt("line break length"))
+            );
+        }
+    }
+
+    public string AdditionalDescription =>
+        "Uses CASSIE to make an animated broadcast - if there is CASSIE playing, it will be stopped. " +
+        "Keep custom formatting to a minimum, this system is very limited.";
+
+    public FrameworkBridge.Type DependsOn => FrameworkBridge.Type.Exiled;
+}
