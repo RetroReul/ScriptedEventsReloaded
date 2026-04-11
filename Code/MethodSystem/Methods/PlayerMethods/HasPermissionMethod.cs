cd2204ff6a0e4731eada8968a0b80299ddddb78b
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-11 10:30:27 +0200

    Create HasPermissionMethod.cs

---
+++ Code/MethodSystem/Methods/PlayerMethods/HasPermissionMethod.cs
@@ -0,0 +1,31 @@
+using JetBrains.Annotations;
+using LabApi.Features.Permissions;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.PlayerMethods;
+
+[UsedImplicitly]
+public class HasPermissionMethod : ReturningMethod<BoolValue>
+{
+    public override string Description => "Checks if a player has a specific permission.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new PlayerArgument("player"),
+        new TextArgument("permissions")
+        {
+            ConsumesRemainingValues = true,
+            Description = "'true' is returned when player has any of the provided permissions"
+        }
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = Args
+            .GetPlayer("player")
+            .HasAnyPermission(Args.GetRemainingArguments<string, TextArgument>("permissions"));
+    }
+}
