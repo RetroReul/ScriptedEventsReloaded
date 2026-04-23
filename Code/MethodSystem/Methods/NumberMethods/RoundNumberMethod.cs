b48c4fbac8e34681b2f764d8c5fc1157b47cdfaa
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-23 20:16:06 +0200

    Create RoundNumberMethod.cs

---
+++ Code/MethodSystem/Methods/NumberMethods/RoundNumberMethod.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.NumberMethods;
+
+[UsedImplicitly]
+public class RoundNumberMethod : ReturningMethod<NumberValue>
+{
+    public override string Description => "Rounds a provided number.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new FloatArgument("number to round"),
+        new IntArgument("decimal places", 0)
+        {
+            DefaultValue = new(0, "rounds to whole number (0)"),
+            Description = "The number of decimal places to round to."
+        }
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = (decimal)Math.Round(Args.GetFloat("number to round"), MidpointRounding.AwayFromZero);
+    }
+}
