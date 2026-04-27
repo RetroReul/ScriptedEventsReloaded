2d7ba0420926de891642e7de4eb0c4d63eec4323
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-27 23:08:34 +0200

    Create WaveRespawnTokensMethod.cs

---
+++ Code/MethodSystem/Methods/RespawnMethods/WaveRespawnTokensMethod.cs
@@ -0,0 +1,36 @@
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+
+namespace SER.Code.MethodSystem.Methods.RespawnMethods;
+
+[UsedImplicitly]
+public class WaveRespawnTokensMethod : SynchronousMethod
+{
+    public override string Description => "Changes respawn tokens of a wave.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new WaveTypeArgument("wave type"),
+        new OptionsArgument("mode", "set", "changeBy"),
+        new IntArgument("respawn tokens")
+    ];
+
+    public override void Execute()
+    {
+        if (WaveTypeArgument.GetWave(Args.GetWaveType("wave type")) is not {} wave)
+        {
+            return;
+        }
+        
+        if (Args.GetOption("mode") is "set")
+        {
+            wave.RespawnTokens = Args.GetInt("respawn tokens");
+        }
+        else
+        {
+            wave.RespawnTokens += Args.GetInt("respawn tokens");
+        }
+    }
+}
