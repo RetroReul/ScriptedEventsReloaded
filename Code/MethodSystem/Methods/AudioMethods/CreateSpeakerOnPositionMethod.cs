a126e24cbe98a7d2eaccffb2787a1b1c6c810f52
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-30 11:26:42 +0200

    Create CreateSpeakerOnPositionMethod.cs

---
+++ Code/MethodSystem/Methods/AudioMethods/CreateSpeakerOnPositionMethod.cs
@@ -0,0 +1,68 @@
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Extensions;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+
+namespace SER.Code.MethodSystem.Methods.AudioMethods;
+
+[UsedImplicitly]
+public class CreateSpeakerOnPositionMethod : SynchronousMethod
+{
+    public override string Description => "Creates a speaker in a specified XYZ position.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new TextArgument("speaker name"),
+        new FloatArgument("x"),
+        new FloatArgument("y"),
+        new FloatArgument("z"),
+        new FloatArgument("volume", 0f)
+        {
+            DefaultValue = new(1f, "100%"),
+            Description = "The volume of the audio."
+        },
+        new FloatArgument("min distance", 0f)
+        {
+            DefaultValue = new(5f, null),
+            Description = "The minimum distance for full-volume audio."
+        },
+        new FloatArgument("max distance", 0f)
+        {
+            DefaultValue = new(15f, null),
+            Description = "The maximum audible distance for the audio."
+        },
+        new BoolArgument("is stereo")
+        {
+            DefaultValue = new(true, null),
+            Description = "Whether the audio will be 3D."
+        },
+        new PlayersArgument("target players")
+        {
+            DefaultValue = new(null, "all"),
+            Description = "If specified, only the provided players will hear the audio."
+        }
+    ];
+
+    public override void Execute()
+    {
+        var speakerName = Args.GetText("speaker name");
+        var x = Args.GetFloat("x");
+        var y = Args.GetFloat("y");
+        var z = Args.GetFloat("z");
+        var volume = Args.GetFloat("volume");
+        var minDistance = Args.GetFloat("min distance");
+        var maxDistance = Args.GetFloat("max distance");
+        var isStereo = Args.GetBool("is stereo");
+        var targetPlayers = Args.GetPlayers("target players").MaybeNull();
+        
+        AudioPlayer.Create(
+            speakerName, 
+            condition: hub => targetPlayers is null || targetPlayers.Any(p => p.ReferenceHub == hub),
+            onIntialCreation: p =>
+            {        
+                var speaker = p.AddSpeaker("Main", volume, isStereo, minDistance, maxDistance);
+                speaker.Position = new(x, y, z);
+            }
+        );
+    }
+}
